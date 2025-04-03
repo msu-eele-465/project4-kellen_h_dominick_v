ef3fe8c4df031e1cec872106b8dd4c61ea049043
Kellen <142949567+Hurlicane24@users.noreply.github.com>
2025-04-02 22:08:27 -0600

    Added function/variable declarations

--- controller/app/main.c
+++ controller/app/main.c
@@ -1,22 +1,28 @@
-#include <msp430fr2355.h>
+#include "intrinsics.h"
+#include "msp430fr2355.h"
+#include <msp430.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;
+//Function Declarations
+void unlock_loop();
+void keypad_setup();
+void heartbeat_setup();
+bool arrays_equal(char[], char[]);
+void key_released(volatile unsigned char*, unsigned char);
+void RGB_LED_setup();
+void red_LED();
+void yellow_LED();
+void green_LED();
+void color_change(int, int, int);
+void master_setup(void);
+void write_to_slave(unsigned char, volatile unsigned char[]);
 
-    P1OUT &= ~BIT0;
-    P1DIR |= BIT0;
+unsigned char SLAVE_ADDRESS_1 = 0x45;
+unsigned char SLAVE_ADDRESS_2 = 0x46;
+volatile unsigned char data[2] = {0, 0};
+int BYTES = 2;
+int dataCnt = 0;
+int i;
 
-    // Disable low-power mode / GPIO high-impedance
-    PM5CTL0 &= ~LOCKLPM5;
-
-    while (true)
-    {
-        P1OUT ^= BIT0;
-
-        // Delay for 100000*(1/MCLK)=0.1s
-        __delay_cycles(100000);
-    }
-}
